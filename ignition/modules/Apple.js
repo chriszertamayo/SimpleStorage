76915a160ebc375122fbd51d59bfca285b8afdbc
chriszer tamayo <chriszertamayo@chriszers-MacBook-Pro.local>
2024-11-12 11:11:25 +0800

    first commit

---
+++ ignition/modules/Apple.js
@@ -0,0 +1,7 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+module.exports = buildModule("AppleModule", (m) => {
+  const appleContract = m.contract("Apple");
+
+  return { appleContract };
+});
